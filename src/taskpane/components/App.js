0791c17a893f915f181aaa92bec209863c79b039
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-02-18 14:07:27 +0600

    5th

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -24,47 +24,87 @@ const App = ({ title, isOfficeInitialized }) => {
     },
   ];
 
+  const insertOnlineVideo = (vdName, vdHtml, vdWidth, vdHeight) => {
+    if (Office.CoercionType.Ooxml) {
+      var xmlCode = replaceCode(vdName, escapeHtml(vdHtml), vdWidth, vdHeight);
+      Office.context.document.setSelectedDataAsync(
+        xmlCode,
+        { coercionType: "ooxml" }
+      );
+    }
+  }
+
+  const replaceCode = () => {
+    var i = 0;
+    var args = arguments;
+    var str = $('#xmlcode').val();
+    return str.replace(/@@/g, function () { return args[i++]; });
+  }
+
+  //Escape HTML
+  const escapeHtml = (str) => {
+    var elm = document.createElement('pre');
+    if (typeof elm.textContent != 'undefined') {
+      elm.textContent = str;
+    } else {
+      elm.innerText = str;
+    }
+    return elm.innerHTML.replace(/["']/g, '&quot;');
+  }
 
   const click = async () => {
     return Word.run(async (context) => {
 
-      const insertOnlineVideo = (vdName, vdHtml, vdWidth, vdHeight) => {
-        if (Office.CoercionType.Ooxml) {
-          var xmlCode = replaceCode(vdName, escapeHtml(vdHtml), vdWidth, vdHeight);
-          Office.context.document.setSelectedDataAsync(
-            xmlCode,
-            { coercionType: "ooxml" }
-          );
-        }
-      }
 
 
-      var vdName = "My Video";
-      var vdHtml = '<iframe width="800" height="600" src="http://www.youtube.com/embed/qk51u8-4uo4" frameborder="0" allowfullscreen></iframe>'; //YouTube embed video code
-      //var vdHtml = '<object width="800" height="600"><param name="movie" value="http://i.d.com.com/av/video/embed/player.swf" /><param name="background" value="#333333" /><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="true" /><param name="FlashVars" value="playerType=embedded&type=id&value=50139900" /><embed src="http://www.cnet.com/av/video/embed/player.swf" type="application/x-shockwave-flash" background="#333333" width="800" height="600" allowFullScreen="true" allowScriptAccess="always" FlashVars="playerType=embedded&type=id&value=50139900" /></object>'; //CNET's embed video code
-      var vdWidth = 800;
-      var vdHeight = 600;
-      insertOnlineVideo(vdName, vdHtml, vdWidth, vdHeight);
+      // insert video
+      // var vdName = "My Video";
+      // var vdHtml = '<iframe width="800" height="600" src="http://www.youtube.com/embed/qk51u8-4uo4" frameborder="0" allowfullscreen></iframe>'; //YouTube embed video code
+      // var vdWidth = 800;
+      // var vdHeight = 600;
+      // insertOnlineVideo(vdName, vdHtml, vdWidth, vdHeight);
 
-      const replaceCode = () => {
-        var i = 0;
-        var args = arguments;
-        var str = $('#xmlcode').val();
-        return str.replace(/@@/g, function () { return args[i++]; });
-      }
+      // insert image
+      var body = context.document.body;
 
-      //Escape HTML
-      const escapeHtml = (str) => {
-        var elm = document.createElement('pre');
-        if (typeof elm.textContent != 'undefined') {
-          elm.textContent = str;
-        } else {
-          elm.innerText = str;
-        }
-        return elm.innerHTML.replace(/["']/g, '&quot;');
-      }
+      //for online img
+      // body.insertHtml(
+      //   '<img src="https://i.insider.com/60ae5f85bee0fc0019d598e0?width=700" alt="Girl in a jacket" width="100" height="100">', Word.InsertLocation.start);
 
+      // from src image
+      body.insertHtml(
+        `<img src=${require("./../../../assets/logo-filled.png")} alt="Girl in a jacket" width="100" height="100">`, Word.InsertLocation.start);
 
+      // inert text
+
+      // Create a proxy object for the document body.
+      // var body = context.document.body;
+      // body.insertHtml(
+      //   '<strong>This is text inserted with body.insertHtml()</strong>', Word.InsertLocation.start);
+
+      // insert paragraph
+      // let secondSentence = context.document.body.insertParagraph(
+      //   "This is the first text with a custom style.",
+      //   "End"
+      // );
+      // secondSentence.font.set({
+      //   bold: false,
+      //   italic: true,
+      //   name: "Berlin Sans FB",
+      //   color: "blue",
+      //   size: 30
+      // });
+
+      // insert table
+
+      // Use a two-dimensional array to hold the initial table values.
+      // let data = [
+      //   ["Tokyo", "Beijing", "Seattle"],
+      //   ["Apple", "Orange", "Pineapple"]
+      // ];
+      // let table = context.document.body.insertTable(2, 3, "Start", data);
+      // table.styleBuiltIn = Word.Style.gridTable5Dark_Accent2;
+      // table.styleFirstColumn = false;
 
       await context.sync();
     });
